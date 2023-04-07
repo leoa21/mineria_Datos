b1c67c4dfafc2ac03633f0ea175324361e1d6b2c
Leo <leo.21.alvarez@hotmail.com>
2023-04-06 23:26:59 -0600

    Practica 1

---
+++ PreprocesamientoDatos.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+
+df = pd.read_csv('csv/results.csv')
+
+#Cambiando el nombre a las columnas
+columnas = ['Fecha', 'Local', 'Visitante', 'Goles_Local',
+            'Goles_Visitantes', 'Torneo', 'Ciudad', 'Pais', 'Neutral']
+
+df.columns = columnas
+
+df['Fecha'] = pd.to_datetime(df['Fecha'])
+
+df.info()
+
+print(df)
